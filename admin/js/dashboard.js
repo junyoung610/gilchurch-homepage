752bda72b28be051a6f4c41e4f74df77ae34e7ef
junyoung610 <junyoungkim610@gmail.com>
2026-06-18 00:54:42 +0900

    260618(2)_uplode

---
+++ admin/js/dashboard.js
@@ -0,0 +1,9 @@
+import { auth } from "./firebase.js";
+
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    location.href = "/admin/login.html";
+  }
+});
--- js/auth.js
+++ js/auth.js
@@ -1,31 +1,28 @@
-import { auth, db } from "./firebase.js";
-
-import {
-  GoogleAuthProvider,
-  signInWithPopup,
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-
-import { doc, setDoc } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-
-const loginBtn = document.getElementById("googleLogin");
-
 loginBtn.addEventListener("click", async () => {
-  const provider = new GoogleAuthProvider();
+  try {
+    const provider = new GoogleAuthProvider();
 
-  const result = await signInWithPopup(auth, provider);
+    const result = await signInWithPopup(auth, provider);
 
-  const user = result.user;
+    const user = result.user;
 
-  await setDoc(
-    doc(db, "users", user.uid),
-    {
-      uid: user.uid,
-      name: user.displayName,
-      email: user.email,
-      role: "superAdmin",
-    },
-    { merge: true },
-  );
+    await setDoc(
+      doc(db, "users", user.uid),
+      {
+        uid: user.uid,
+        name: user.displayName,
+        email: user.email,
+        role: "superAdmin",
+      },
+      { merge: true },
+    );
 
-  location.href = "/admin/dashboard.html";
+    alert("로그인 성공");
+
+    location.href = "/gilchurch-homepage/admin/dashboard.html";
+  } catch (error) {
+    console.error(error);
+
+    alert(error.message);
+  }
 });
