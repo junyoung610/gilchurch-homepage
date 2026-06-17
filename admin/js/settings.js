ee9a2b4be286d5cfcac5fd1fe53f71fdd30228f2
junyoung610 <junyoungkim610@gmail.com>
2026-06-18 01:34:54 +0900

    260618(7)_uplode

--- admin/js/settings.js
+++ admin/js/settings.js
@@ -1,3 +1,11 @@
+import { storage } from "../../js/firebase.js";
+
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
+
 import { db } from "../../js/firebase.js";
 
 import {
@@ -29,12 +37,54 @@ async function loadSettings() {
 
 loadSettings();
 
-//저장기능
+// 파일요소가져오기
+const logoFile = document.getElementById("logoFile");
+
+const faviconFile = document.getElementById("faviconFile");
+
+const ogFile = document.getElementById("ogFile");
+
+// 업로드함수만들기
+async function uploadFile(file, path) {
+  if (!file) return null;
+
+  const storageRef = ref(storage, path);
+
+  await uploadBytes(storageRef, file);
+
+  return await getDownloadURL(storageRef);
+}
+
+// 저장버튼
 saveBtn.addEventListener("click", async () => {
-  await updateDoc(doc(db, "settings", "site"), {
+  let logoUrl = null;
+  let faviconUrl = null;
+  let ogUrl = null;
+
+  if (logoFile.files[0]) {
+    logoUrl = await uploadFile(logoFile.files[0], "logos/logo.png");
+  }
+
+  if (faviconFile.files[0]) {
+    faviconUrl = await uploadFile(faviconFile.files[0], "favicon/favicon.ico");
+  }
+
+  if (ogFile.files[0]) {
+    ogUrl = await uploadFile(ogFile.files[0], "og-images/og-image.jpg");
+  }
+
+  const updateData = {
     siteName: siteName.value,
     description: description.value,
-  });
+  };
+
+  if (logoUrl) updateData.logo = logoUrl;
+
+  if (faviconUrl) updateData.favicon = faviconUrl;
+
+  if (ogUrl) updateData.ogImage = ogUrl;
+
+  await updateDoc(doc(db, "settings", "site"), updateData);
 
   alert("저장 완료");
 });
--- js/firebase.js
+++ js/firebase.js
@@ -4,6 +4,8 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth
 
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCr_EQzXssO2BeXcFl9_NZeagD7pGKSBX0",
   authDomain: "gilchurch-homepage.firebaseapp.com",
@@ -18,3 +20,5 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+export const storage = getStorage(app);
