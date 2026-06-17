ec780c2d1377792cc8e3526812cfbe0eb68e7731
junyoung610 <junyoungkim610@gmail.com>
2026-06-18 01:12:42 +0900

    260618(5)_uplode

--- admin/js/dashboard.js
+++ admin/js/dashboard.js
@@ -1,9 +1,30 @@
-import { auth } from "./firebase.js";
+import { auth, db } from "../../js/firebase.js";
 
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-onAuthStateChanged(auth, (user) => {
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+onAuthStateChanged(auth, async (user) => {
   if (!user) {
     location.href = "/gilchurch-homepage/admin/login.html";
+
+    return;
   }
+
+  const userRef = doc(db, "users", user.uid);
+
+  const userSnap = await getDoc(userRef);
+
+  if (!userSnap.exists()) {
+    alert("권한 없음");
+
+    return;
+  }
+
+  const data = userSnap.data();
+
+  document.getElementById("userInfo").innerHTML = `
+        <p>${data.name}님 환영합니다.</p>
+        <p>권한 : ${data.role}</p>
+    `;
 });
