849f401bb9961ca95b4b84fcc0c4cc6a429248c9
junyoung610 <junyoungkim610@gmail.com>
2026-06-19 00:53:15 +0900

    260618(26)_uplode

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,9 +1,52 @@
-import { auth } from "./firebase.js";
+import { db, auth } from "./firebase.js";
 
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import {
+  doc,
+  getDoc,
+  collection,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-onAuthStateChanged(auth, (user) => {
+import { signOut } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+onAuthStateChanged(auth, async (user) => {
   if (!user) {
     location.href = "/gilchurch-homepage/admin/login.html";
+
+    return;
   }
+
+  const userRef = doc(db, "users", user.uid);
+
+  const userSnap = await getDoc(userRef);
+
+  if (!userSnap.exists()) {
+    alert("권한 없음");
+
+    return;
+  }
+
+  const data = userSnap.data();
+
+  document.getElementById("userInfo").innerHTML = `
+
+  <div class="user-name">
+    ${data.name}
+  </div>
+
+  <div class="user-role">
+    ${data.role}
+  </div>
+
+`;
 });
+
+const pageCount = (await getDocs(collection(db, "pages"))).size;
+
+document.getElementById("pageCount").textContent = pageCount;
+
+document.getElementById("logoutBtn").addEventListener("click", async () => {
+  await signOut(auth);
+
+  location.href = "./login.html";
+}); // 수정
